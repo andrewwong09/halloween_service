58bd1616e915f212c00b03346f77e082231a95eb
andrew <andrewwong09@gmail.com>
2022-10-18 09:22:22 -0700

    removed unused play_music function

--- hello_world.py
+++ hello_world.py
@@ -19,23 +19,6 @@ def in_between(now, start=datetime.time(16, 30), end=datetime.time(19, 30)):
         return start <= now or now < end
 
 
-def play_music():
-    while(1):
-        songs = [s for s in os.listdir('/home/andrew/Music/') if '.mp3' in s]
-        for s in songs:
-            print(s)
-            vol = 80
-            if 'vol' in s:
-                vol = int(s.split('vol')[1][:3])
-            os.system(f'amixer cset numid=1 {vol}%')
-            player = vlc.MediaPlayer(f'/home/andrew/Music/{s}')
-            player.play()
-            time.sleep(0.5)
-            duration = player.get_length()
-            print(duration)
-            time.sleep(duration)
-
-
 def play_sound():
     with open(sound_process_file, 'r+') as f:
         first_line = f.readline()
@@ -72,5 +55,4 @@ if __name__ == '__main__':
 
     p = Process(target=ca.start)
     p.start()
-    #play_music()
-    p.join()
+    p.join() # This will likely never be reached
