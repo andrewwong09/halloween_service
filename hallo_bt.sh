048e209ab8f80cae0fbedb9da9f0e2cadb5e31fa
andrew <andrewwong09@gmail.com>
2022-10-18 16:38:03 -0700

    Ensure both stderr and stdout append to log file

--- hallo_bt.sh
+++ hallo_bt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "\n$(date): BT Buzz-------------------------------" >> /home/andrew/scripts/logs/halloween.service.log 
-/home/andrew/directory_env/hallo/bin/python /home/andrew/scripts/bt_buzz.py &>> /home/andrew/scripts/logs/halloween.service.log 
+echo -e "\n$(date): BT Buzz-------------------------------" >> /home/andrew/scripts/logs/halloween.service.log 
+/home/andrew/directory_env/hallo/bin/python /home/andrew/scripts/bt_buzz.py >> /home/andrew/scripts/logs/halloween.service.log 2>&1 
 
