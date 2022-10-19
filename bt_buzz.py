cd2c1580224ad72902b5d725eb6b5bd7d102ab9d
andrew <andrewwong09@gmail.com>
2022-10-19 09:35:38 -0700

    Added duration check to fix bug of bt_buzz not restarting at anticipated interval because duration check of song is wrong.

--- bt_buzz.py
+++ bt_buzz.py
@@ -1,21 +1,34 @@
 import os
 import time
+import logging
+
 import vlc
 
+
+logging.basicConfig(filename='/home/andrew/scripts/logs/bt_buzz.log', 
+                    format='%(asctime)s %(message)s', 
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    encoding='utf-8',
+                    level=logging.DEBUG)
 
 os.system('amixer controls')
 
 
 def play_sound():
-    vol = 10
+    vol = 60
     os.system(f'amixer cset numid=1 {vol}%')
     player = vlc.MediaPlayer(f'/home/andrew/Music/thunder0.mp3')
     player.play()
     time.sleep(0.5)
     duration = player.get_length()
-    time.sleep(duration)
+    if duration > 30:
+        time.sleep(30)
+    else:
+        time.sleep(duration)
     player.stop()
 
 
 if __name__ == '__main__':
+    logging.info(f'bt_buzz in')
     play_sound()
+    logging.info(f'bt_buzz out')
