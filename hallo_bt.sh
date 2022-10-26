64859ad5d51744f6a572849013d924b53336f863
andrew <andrewwong09@gmail.com>
2022-10-26 13:52:30 -0700

    added command lines to check if bluetooth devices are connected and attempted connection. The bluetooth device check doesn't seem to be outputting as expected in the log files.

--- hallo_bt.sh
+++ hallo_bt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo -e "\n$(date): BT Buzz-------------------------------" >> /home/andrew/scripts/logs/halloween.service.log 
+echo -e "\n$(date): BT Buzz-------------------------------" >> /home/andrew/scripts/logs/halloween.service.log
+
+bluetoothctl devices | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done|grep -e "Device\|Connected\|Name"
+
 /home/andrew/directory_env/hallo/bin/python /home/andrew/scripts/bt_buzz.py >> /home/andrew/scripts/logs/halloween.service.log 2>&1 
 
--- hallo_exec.sh
+++ hallo_exec.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo -e "\n$(date) Hallo World----------------------------" >> /home/andrew/scripts/logs/halloween.service.log 
+echo -e "\n$(date) Hallo World----------------------------" >> /home/andrew/scripts/logs/halloween.service.log
+
+sleep 10
+
+bluetoothctl connect F4:4E:FD:52:5C:A9 >> /home/andrew/scripts/logs/halloween.service.log 2>&1
+
 /home/andrew/directory_env/hallo/bin/python /home/andrew/scripts/hallo_world.py >> /home/andrew/scripts/logs/halloween.service.log 2>&1
 
